d1c77298d702adf8951297e030d40e8c865719d8
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-09-17 18:33:10 +0530

    fix: update CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,12 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+console.log(process.env.CORS_ORIGIN);
+
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
 app.use(express.json({ limit: "16kb" }));
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,8 +4,8 @@ import { ApiResponse } from "../utils/apiResponse.js";
 
 const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000,
 };
 
