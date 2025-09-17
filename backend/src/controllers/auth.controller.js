3406d48e582e49dc77b27c93c97db024a6261d85
Mitesh-Kumavat <miteshkumavat777@gmail.com>
2025-09-17 17:27:44 +0530

    fix: cookie issue in deployment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -21,8 +21,8 @@ export const registerUser = async (req, res) => {
 
         res.cookie("authToken", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
@@ -61,8 +61,8 @@ export const loginUser = async (req, res) => {
 
         res.cookie("authToken", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
